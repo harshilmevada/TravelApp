0f2fb28acae4e94b6f38aaecb65fa1522edb21d8
Mevada Harshil Jayshbhai <95343203+harshilmevada@users.noreply.github.com>
2024-02-26 09:56:23 +0530

    Update buzy_time.py
    add comments to inform someone.

--- CLI/buzy_time.py
+++ CLI/buzy_time.py
@@ -11,15 +11,19 @@ cur_time = current_time_as_integer()
 
 
 def buzyTime():
-      # 7:30 to 10:30
+      # 7:30 to 10:30(am)
       if 730 <= cur_time <= 1030:
             return 33
+      # 10:31 to 4:30(am:pm)
       elif 1031 <= cur_time <= 1630:
            return -3
+      # 4:31 to 6:00(pm)
       elif 1631 <= cur_time <= 1800:
            return 11
+      # 6:01 to 9:00(pm)
       elif 1801 <= cur_time <= 2100:
            return 33
+      # 9:01 to 7:29(pm:am)
       else:
            return -33
 
